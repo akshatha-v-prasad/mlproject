164ad6130dd950bf7087d10aef9a2422229da369
akshatha-v-prasad <akshathav1@gmail.com>
2023-07-01 23:20:52 +0200

    Linear reg model

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,12 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -36,5 +42,11 @@ class DataIngestion:
             raise CustomException(e,sys)
 
 if __name__=="__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
